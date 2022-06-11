d0a0d8bd00304af26375870957a0c98224cd40e1
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-11 13:35:30 -0300

    Added dataset

---
+++ classifier.js
@@ -0,0 +1,23 @@
+// Read dataset file
+
+const logDataset = () => {
+    var mydata = JSON.parse(data);
+
+
+    mydata.forEach(element => {
+        console.log(element);
+    });
+
+};
+
+// Training phase
+
+
+// Receives input
+
+
+// Perform classification
+
+
+// Show result
+
