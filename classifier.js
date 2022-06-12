f0915bc75317ddc902f7cb201386994bbd553557
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-11 21:18:46 -0300

    Display result logic

--- classifier.js
+++ classifier.js
@@ -1,11 +1,13 @@
-// Read dataset file
-const dataSet = JSON.parse(data);
-
 // Training phase
 const trainClassifierModel = () => {
-    dataSet.forEach(mushroom => {
-        console.log(mushroom);
-    });
+    // Read dataset file
+    const dataSet = JSON.parse(data);
+
+    // dataSet.forEach(mushroom => {
+    //     console.log(mushroom);
+    // });
+
+    console.log("Training classifier...");
 };
 
 // Receives input
@@ -22,7 +24,24 @@ const retrieveMushroomData = () => {
 };
 
 // Perform classification
+const classifyAndReturnResult = () => {
+    let mushroom = retrieveMushroomData();
 
+    // classification logic
+
+    // return result (edible (1) - poisonous (0))
+    return 0;
+};
 
 // Show result
+const classifyMushroom = () => {
+    let classification = classifyAndReturnResult();
 
+    document.getElementById('classificationResult').innerHTML = "Result: " + (classification == 1 ? 'edible' : 'poisonous');
+    document.getElementById('classificationResult').style.display = 'block';
+};
+
+
+window.onload = function () {
+    trainClassifierModel();
+};
