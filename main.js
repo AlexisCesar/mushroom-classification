66fb8e90d1960fa9e085a3517ba1369d8e9320a0
Flavio Moreira Terra <flavio.moreira@venturus.org.br>
2022-06-19 16:19:38 -0300

    style carousel done

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
     //btn event appear
     let btn_main = document.getElementById('btn-main');
 
-    btn_main.classList.add('active');
+    if (btn_main != null) {
+        btn_main.classList.add('active');
 
-    //title btn appear
-    document.getElementById('title-main').classList.add('active');
+        //title btn appear
+        document.getElementById('title-main').classList.add('active');
+    }
 
     //background event appear
     setTimeout(function() {
@@ -13,4 +15,12 @@ document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('stars').classList.add('active');
         document.getElementById('stars2').classList.add('active');
     }, 1000);
+
+    const form = document.querySelector("#title-main")
+
+    if (form != null) {
+        form.addEventListener('click', () => {
+            location.href = "./form.html"
+        })
+    }
 });
